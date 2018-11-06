4ac7ac2538ce81ea88daeb3758b05d7e8e3c23c3
parasquid <tristan.gomez@gmail.com>
2018-11-06 19:13:40 +0800

    add minor styling, fix connection when there are old cookies

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,7 +3,7 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = User.find(cookies.signed[:user_id])
+      self.current_user = User.where(id: cookies.signed[:user_id]).first
       reject_unauthorized_connection unless current_user
     end
   end
